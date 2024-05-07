4ee5e1e3a069037861596d269ee65d35f896dec3
Marat <milya010790@mail.ru>
2024-05-07 21:36:56 +0500

    переопределил выборки

--- DigitalLibrary.DAL/Repositories/BookRepository.cs
+++ DigitalLibrary.DAL/Repositories/BookRepository.cs
@@ -1,11 +1,15 @@
 ﻿using DigitalLibrary.DAL.Context;
 using DigitalLibrary.DAL.Entityes;
 using DigitalLibrary.DAL.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace DigitalLibrary.DAL.Repositories
 {
     public class BookRepository : BaseRepository<Book>
     {
-        public BookRepository(ApplicationContext db) : base(db) { }
+        public override IQueryable<Book> Items => base.Items.Include(item => item.Users);
+
+
+        public BookRepository(ApplicationContext db) : base(db) { } 
     }
 }
--- DigitalLibrary.DAL/Repositories/UserRepository.cs
+++ DigitalLibrary.DAL/Repositories/UserRepository.cs
@@ -1,11 +1,14 @@
 ﻿using DigitalLibrary.DAL.Context;
 using DigitalLibrary.DAL.Entityes;
 using DigitalLibrary.DAL.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace DigitalLibrary.DAL.Repositories
 {
     public class UserRepository : BaseRepository<User>
     {
+        public override IQueryable<User> Items => base.Items.Include(item => item.Books);
+
         public UserRepository(ApplicationContext db) : base(db) { }
     }
 }
