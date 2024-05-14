472be86677bba1d936f1d3962e1032f08913ff42
Marat <milya010790@mail.ru>
2024-05-14 23:21:14 +0500

    Добавил дополнительные методы в репозиторий

--- DigitalLibrary.DAL/Repositories/BookRepository.cs
+++ DigitalLibrary.DAL/Repositories/BookRepository.cs
@@ -10,6 +10,48 @@ namespace DigitalLibrary.DAL.Repositories
         public override IQueryable<Book> Items => base.Items.Include(item => item.Users);
 
 
-        public BookRepository(ApplicationContext db) : base(db) { } 
+        public BookRepository(ApplicationContext db) : base(db) { }
+
+        /// <summary>Получать список книг определенного жанра и вышедших между определенными годами.</summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public IEnumerable<Book> GetGenreBetweenDates(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Метод получения количество книг определенного автора </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int GrtCountCertainAuthor()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Есть ли книга определенного автора и с определенным названием в библиотеке.</summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool IsBookCertainAuthor()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Получение последней вышедшей книги.</summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Book LastBookPublished()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Получение списка всех книг, отсортированного в алфавитном порядке по названию.</summary>
+        /// <returns></returns>
+        public IEnumerable<Book> AllBooksTitleAsc() => Items.OrderBy(item => item.Title);
+
+        /// <summary>Получение списка всех книг, отсортированного в порядке убывания года их выхода</summary>
+        /// <returns></returns>
+        public IEnumerable<Book> AllBooksYearReleaseDesc() => Items.OrderByDescending(item => item.YearRelease);
     }
 }
