a3acf5af5a91607a2ded6694a4f4af6e6ca301ba
Marat <milya010790@mail.ru>
2024-05-15 19:04:30 +0500

    Добавление слоя бизнес логики

---
+++ DigitalLibrary.BLL/Models/BookModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLibrary.BLL.Models
+{
+    internal class BookModel
+    {
+        /// <summary>Идентификатор</summary>
+        public int Id { get; set; }
+
+        /// <summary>Название книги</summary>
+        public string Title { get; set; }
+
+        /// <summary> Год выпуска книги</summary>
+        public int YearRelease { get; set; }
+
+        /// <summary>Автор книги </summary>
+        public string? Author { get; set; }
+
+        /// <summary>Жанр</summary>
+        public string? Genre { get; set; }
+
+        /// <summary>Список у кого книги</summary>
+        public virtual IEnumerable<UserModel> Users { get; set; }
+    }
+}
---
+++ DigitalLibrary.BLL/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalLibrary.BLL.Models
+{
+    internal class UserModel
+    {
+        /// <summary>Идентификатор</summary>
+        public int Id { get; set; }
+
+        /// <summary>Имя пользователя</summary>
+        public string Name { get; set; }
+
+        /// <summary>Электронный адрес</summary>
+        public string Email { get; set; }
+
+        /// <summary>Книги на руках</summary>
+        public virtual IEnumerable<BookModel> Books { get; set; }
+    }
+}
